990fc0405867e40accc333c4bbb242174a1dc45f
George Achilleos <gachil01@cs.ucy.ac.cy>
2018-12-03 17:28:10 +0200

    save.php

---
+++ create.php
@@ -0,0 +1,30 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'cs425_hw04');
+ 
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$Name = $_POST['Name'];
+$Location = $_POST['Location'];
+$Operator = $_POST['Operator'];
+$CommisionDate = $_POST['CommissionDate'];
+$Description = $_POST['Description'];
+$SystemPower = $_POST['SystemPower'];
+$AnnualProduction = $_POST['AnnualProduction'];
+$CO2Avoided = $_POST['CO2Avoided'];
+$Reimbursement = $_POST['Reimbursement'];
+$Panel = $_POST['Panel'];
+$Azimuth = $_POST['Azimuth'];
+$Inclination = $_POST['Inclination'];
+$Communication = $_POST['Communication'];
+$Inverter = $_POST['Inverter'];
+$Sensors = $_POST['Sensors'];
+
+$sql = "INSERT INTO pv_system (Name,Location,Operator,Commision date, Description, System Power, Annual Production, CO2 Avoided, Reimbursement, Solar Panel Modules, Azimuth Angle, Inclination Angle, Communication, Inverter, Sensors) VALUES ['$Name','$Location','$Operator','$CommisionDate','$Description','$SystemPower','$AnnualProduction','$CO2Avoided','$Reimbursement','$Panel','$Azimuth','$Inclination','$Communication','$Inverter', $Sensors]";
+?>
